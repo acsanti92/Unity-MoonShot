e441f5168f3ead51b81b4e98a02d93ab5a9985e1
acsanti92 <armando.santiago@pluto.tv>
2023-02-23 11:30:13 -0800

    Add .gitignore file

---
+++ Assets/Moon/MoonCode.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86fc308b6911ed73ac8b6ae87dab7b472c4a38f3781d3e79d54caa92f00069c6
+size 159
---
+++ Assets/ShadowVision/Generic/Scripts/WaveUtils.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7130fd1b496d2e234efdd1b4bf0628bdfa52a74a8949bb2fb94d8442e1ee902d
+size 2694
